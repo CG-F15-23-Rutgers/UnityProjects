fcd845b958095597ba41bfca7fd7852620b333f3
Zac1164 <zacharydaniels@nbp-205-43.nbp.ruw.rutgers.edu>
2015-10-18 00:53:46 -0400

    Started part 3

---
+++ BAssignments/B1/Part3/Assets/Scripts/DevilController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevilController : MonoBehaviour {
+	
+	private Transform pos;
+	private int direction;
+
+	void Start(){
+		Transform pos;
+		pos = GetComponent<Transform>();
+		float zPos = pos.position.z;
+		if (Mathf.Abs(4 - zPos) < Mathf.Abs(-8 - zPos)) {
+			direction = -1;
+		} else {
+			direction = 1;
+		}
+	}
+
+	void Update(){
+		Transform pos;
+		pos = GetComponent<Transform>();
+		float xPos = pos.position.x;
+		float yPos = pos.position.y;
+		float zPos = pos.position.z;
+		if (zPos < -8){
+			direction = 1;
+		}else if (zPos >= 4){
+			direction = -1;
+		}
+		zPos = zPos + (direction * Time.deltaTime);
+
+		transform.position = new Vector3(xPos,yPos,zPos);
+		transform.Rotate(new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
---
+++ BAssignments/B1/Part3/Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DirectorScript : MonoBehaviour {
+    NavMeshAgent[] agentsList;
+
+	// Use this for initialization
+	void Start () {
+        agentsList = GetComponents<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach(NavMeshAgent agent in agentsList)
+            {
+                if(agent.CompareTag("mazeagents"))
+                {
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                    {
+                        agent.destination = hit.point;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            foreach (NavMeshAgent agent in agentsList)
+            {
+                if (agent.CompareTag("stairagents"))
+                {
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                    {
+                        agent.destination = hit.point;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            foreach (NavMeshAgent agent in agentsList)
+            {
+                if (agent.CompareTag("jummpagents"))
+                {
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                    {
+                        agent.destination = hit.point;
+                    }
+                }
+            }
+        }
+    }
+}
