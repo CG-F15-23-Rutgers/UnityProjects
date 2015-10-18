2f11d22b50c4d14f811e6819f3d1bb78a7cccc96
Zac1164 <zacharydaniels@nbp-205-43.nbp.ruw.rutgers.edu>
2015-10-17 21:35:55 -0400

    Updated part 1

---
+++ BAssignments/B1/Part1/Assets/Scripts/DirectorAgentController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DirectorAgentController : MonoBehaviour
+{
+    List<NavMeshAgent> agentsList;
+
+    // Use this for initialization
+    void Start()
+    {
+        agentsList = new List<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Agent" && Input.GetMouseButtonDown(0))
+            {
+                NavMeshAgent tempAgent = (hit.transform.gameObject).GetComponent<NavMeshAgent>();
+                if (!agentsList.Contains(tempAgent))
+                {
+                    agentsList.Add(tempAgent);
+                } 
+            }
+            if (hit.transform.tag == "Ground" && Input.GetMouseButtonDown(1))
+            {
+                foreach (NavMeshAgent agent in agentsList)
+                {
+                    agent.destination = hit.point;
+                }
+                agentsList.Clear();
+            }
+        }
+    }
+
+}
---
+++ BAssignments/B1/Part1/Assets/Scripts/ObstaclesMovementController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclesMovementController : MonoBehaviour {
+    Rigidbody obstacle;
+    public float obstacleSpeed;
+
+	// Use this for initialization
+	void Start () {
+        //obstacle = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Container" && Input.GetMouseButtonDown(0))
+            {
+                obstacle = (hit.transform.gameObject).GetComponent<Rigidbody>();
+            }
+        }
+        if (obstacle != null)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 obstacleMovement = new Vector3(moveHorizontal, 0, moveVertical);
+            obstacle.AddForce(obstacleMovement * obstacleSpeed);
+        }
+    }
+}
