618b742146c42b98c4a4fe734286cc66c9e1cef5
Zac1164 <zacharydaniels@nbp-205-43.nbp.ruw.rutgers.edu>
2015-10-18 11:18:48 -0400

    Working on part 3

--- BAssignments/B1/Part3/Assets/Scripts/DirectorAgentController.cs
+++ BAssignments/B1/Part3/Assets/Scripts/DirectorAgentController.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
+
 public class DirectorAgentController : MonoBehaviour
 {
     List<NavMeshAgent> agentsList;
+
+	[System.NonSerialized]
+	
+	public float animSpeed = 1.5f;              // animation speed
+	public bool curve;                          // curves to fix collider during jump
+	
+	private Animator refPlayerAnim;                         // reference player animator
+	private AnimatorStateInfo refAnimState;         // reference animator state
+	private CapsuleCollider refCollider;                    // reference collider
+
+	static int checkMovement = Animator.StringToHash("Base Layer.Movement");
+	static int checkJump = Animator.StringToHash("Base Layer.Jump");
+
+	public GameObject agentObject;
 
     // Use this for initialization
     void Start()
     {
         agentsList = new List<NavMeshAgent>();
+		// references
     }
 
     // Update is called once per frame
@@ -37,5 +56,31 @@ public class DirectorAgentController : MonoBehaviour
             }
         }
     }
+
+	void FixedUpdate()
+	{
+		refPlayerAnim = agentObject.GetComponent<Animator>();
+		refCollider = agentObject.GetComponent<CapsuleCollider>();
+		if (refPlayerAnim.layerCount == 2) {
+			refPlayerAnim.SetLayerWeight (1, 1);
+		}
+		//float horiz = Input.GetAxis("Horizontal");              // horizontal input axis
+		//float vert = Input.GetAxis("Vertical");             // vertical input axis
+		//refPlayerAnim.SetFloat("Speed", vert);
+		//refPlayerAnim.SetFloat("Direction", horiz);
+		refPlayerAnim.speed = animSpeed;                                // animation speed variable
+		refAnimState = refPlayerAnim.GetCurrentAnimatorStateInfo(0); // set our currentState variable to the current state of the Base Layer (0) of animation
+
+		NavMeshAgent agentMesh = agentObject.GetComponent<NavMeshAgent>();
+		Debug.Log (agentMesh.hasPath);
+
+
+		if (agentMesh.hasPath) {
+			refPlayerAnim.SetFloat("Run", 0);
+		} else {
+			refPlayerAnim.SetFloat("Run", 1);
+		}
+
+	}
 
 }
